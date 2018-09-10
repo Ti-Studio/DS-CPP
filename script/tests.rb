53acfa41b0fcc8f11cd4fe2c2d668166025fdd98
iBug <7273074+iBug@users.noreply.github.com>
2018-09-10 13:04:48 +0800

    Improve test script

--- script/tests.rb
+++ script/tests.rb
@@ -9,11 +9,23 @@ tests = YAML.load_file (if ARGV.length >= 1 then
                         end)
 
 result = 0
+
+cxxflags = if tests.include? 'cxxflags'
+             tests['cxxflags']
+           else
+             []
+           end
+
+# Syntax check
+
+cmd = ['g++', '-fsyntax-only'] + cxxflags
 tests['syntax_check'].each do |f|
-  system 'g++', '-fsyntax-only', f or result = 1
+  puts "Performing syntax check for #{f}"
+  system *cmd, f or result += 1
 end
 
 if result != 0
+  puts "#{result} error#{result > 1 ? 's' : ''} found."
   exit result
 end
 
